cd167555c2bcd02775064d012471fda1b831b487
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2021-05-29 23:53:52 +0530

    Data communication service

--- src/app/shared/service/data-communication.service.ts
+++ src/app/shared/service/data-communication.service.ts
@@ -1,3 +1,15 @@
+/**
+ * © Rolling Array https://rollingarray.co.in/
+ *
+ * long description for the file
+ *
+ * @summary Data communication service
+ * @author code@rollingarray.co.in
+ *
+ * Created at     : 2021-05-29 23:52:55 
+ * Last modified  : 2021-05-29 23:53:50
+ */
+
 import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
