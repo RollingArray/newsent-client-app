584bbb42b9adddab106f169b060fdf5694f741c6
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2021-05-29 23:52:41 +0530

    Alert shared service

--- src/app/shared/service/alert.service.ts
+++ src/app/shared/service/alert.service.ts
@@ -1,3 +1,16 @@
+/**
+ * © Rolling Array https://rollingarray.co.in/
+ *
+ * long description for the file
+ *
+ * @summary Alert service
+ * @author code@rollingarray.co.in
+ *
+ * Created at     : 2021-05-29 23:52:18 
+ * Last modified  : 2021-05-29 23:52:18 
+ */
+
+
 import { Injectable } from "@angular/core";
 import { AlertController } from "@ionic/angular";
 
